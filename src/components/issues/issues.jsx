800df69b79b12ebd037ee43e0299a2168bf8ecbf
Martin Brennan <mjrbrennan@gmail.com>
2015-05-28 22:37:54 +1000

    using bluebird for promises, implemented simple api call and model for issues

--- src/components/issues/issues.jsx
+++ src/components/issues/issues.jsx
@@ -1,8 +1,33 @@
 import React from 'react';
+import IssueModel from 'models/issue';
 
 class Issues extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = { issues: [] };
+  }
+
   render() {
-    return (<p>Issues</p>);
+    return (
+      <div>
+      <h2>Issues</h2>
+      <ul>
+        {this.state.issues.map((issue) => {
+          return <li>
+            <p><strong>{issue.title}</strong></p>
+            <p dangerouslySetInnerHTML={{__html: issue.description_output}}></p>
+          </li>
+        })}
+      </ul>
+    </div>
+    );
+  }
+
+  componentWillMount() {
+    var issue = new IssueModel();
+    issue.all().then((response) => {
+      this.setState({ issues: response.data });
+    }, (response) => { console.log(response) });
   }
 }
 
